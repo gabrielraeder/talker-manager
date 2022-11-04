71fb573016956788d9cf30a4bf08dc15053a9a71
gabrielraeder <gabrielraeder@outlook.com.br>
2022-11-04 14:31:38 -0300

    requisito 02

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -9,4 +9,17 @@ router.get('/', async (req, res) => {
   return res.status(200).json(data);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fs.readByID(+id);
+    if (data) {
+      return res.status(200).json(data);
+    }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } catch (err) {
+    return res.status(500).json({ message: 'Erro interno' });
+  }
+});
+
 module.exports = router;
